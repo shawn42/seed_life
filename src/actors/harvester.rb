aba7b725581be5bcc19ec988aff6c90a42cdd9ec
Shawn Anderson <shawn42@gmail.com>
2013-05-02 12:34:04 -0400

    can now drag-harvest

--- src/actors/harvester.rb
+++ src/actors/harvester.rb
@@ -4,17 +4,30 @@ define_actor :harvester do
     requires :input_manager, :coordinates_translator, :world
     
     setup do
+      actor.has_attributes harvesting: false
+
       input_manager.reg :mouse_down, MsRight do |event|
         pos = coordinates_translator.translate_screen_to_world(vec2(*event[:data]))
         occupant = world.occupant_at(pos.x, pos.y, World::GROUND)
         occupant.react_to :harvest if occupant
+        actor.harvesting = true
       end
 
       input_manager.reg :mouse_motion do |event|
         pos = coordinates_translator.translate_screen_to_world(vec2(*event[:data]))
         occupant = world.occupant_at(pos.x, pos.y, World::SKY)
         occupant.react_to :disperse if occupant
+
+        if actor.harvesting?
+          occupant = world.occupant_at(pos.x, pos.y, World::GROUND)
+          occupant.react_to :harvest if occupant
+        end
       end
+
+      input_manager.reg :mouse_up, MsRight do |event|
+        actor.harvesting = false
+      end
+
 
       reacts_with :remove
     end
